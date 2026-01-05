4a6e161190ae93f11dd90dffae273289e59ba0bd
youssefx99 <yousseffaisal20@gmail.com>
2026-01-05 16:37:52 +0200

    Use terser to remove console.log in production

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    drop: ['console', 'debugger'],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
   },
 });
